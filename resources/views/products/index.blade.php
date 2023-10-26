5a10a45a30e3367886ee0766f374d37c3c0590b8
kuya-Ey <138701056+kuya-Ey@users.noreply.github.com>
2023-10-26 09:59:37 +0800

    sorting

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,8 +1,23 @@
-@foreach ($products as $product)
-    <li>{{ $product->name }}</li>
-    <li>{{ $product->description }}</li>
-    <li>{{ $product->price }}</li>
-    <li>{{ $product->category }}</li>
-    <li>{{ $product->id }}</li>
-@endforeach
+@extends('layouts.app')
+
+@section('content')
+<div class="container d-flex gap-1 flex-wrap">
+    @foreach ($products as $product)
+    <div class="card" style="width: 18rem;">
+        <img src="{{$product->images}}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">{{$product->name}}</h5>
+          <p class="card-text">{{$product->description}}</p>
+          <p class="card-text">{{$product->quantity}}</p>
+          <p class="card-text">{{$product->price}}</p>
+          <p class="card-text">{{$product->category}}</p>
+
+        </div>
+      </div>
+
+
+    @endforeach
+
+</div>
+@endsection
 
